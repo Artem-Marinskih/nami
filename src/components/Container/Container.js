591a53b4bd137dbf2b70548bbcb586af25e9854a
Artem_Marinskih <a.marinskih1@gmail.com>
2022-07-20 22:34:37 +0500

    add useeffect

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,8 +1,12 @@
 import './Container.scss';
 import { array } from '../../pages/MainPage/MainPage';
+import { useState, useEffect } from 'react';
 
 export const Container = ({ id, imgPath, title, text, weight, money }) => {
-  
+  // const arr = [];
+  let x;
+  const [state, setState] = useState(x);
+
   const getOut = () => {
     let productObject = {
       id: id,
@@ -14,9 +18,30 @@ export const Container = ({ id, imgPath, title, text, weight, money }) => {
       count: 1,
     };
 
-    array.push(productObject);
+    console.log('click');
+
+    setState((prev) => ({
+      ...prev,
+      x: productObject,
+    }));
   };
 
+  
+    useEffect(() => {
+      if (state !== undefined) {
+        if (array.find(el => el.id === state.id)) {
+           x.count + 1
+        } else {
+          // let y = array.find(el => el.id === state.id);
+          // console.log(y)
+          array.push(state);
+        }
+      
+    }
+    });
+  
+  
+  // console.log(arr);
 
   return (
     <div className="Container">
